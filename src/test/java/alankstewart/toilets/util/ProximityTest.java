f7d80a893e7e777f2f37e873077b6f0ea98fa310
Alan Stewart <alankstewart@gmail.com>
2015-04-11 12:15:29 +1000

    Used static import

--- src/test/java/alankstewart/toilets/util/ProximityTest.java
+++ src/test/java/alankstewart/toilets/util/ProximityTest.java
@@ -1,7 +1,6 @@
 package alankstewart.toilets.util;
 
 import alankstewart.toilets.model.Location;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -14,8 +13,8 @@ public class ProximityTest {
         Location operaHouse = new Location(-33.856744, 151.215134);
         Location penguinIsland = new Location(-43.349099, 147.371635);
 
-        Assert.assertEquals(6814.3, Proximity.distanceBetween(bondiBeach, operaHouse).inMetres(), 0.1);
-        Assert.assertEquals(1104, Proximity.distanceBetween(bondiBeach, penguinIsland).inKilometres(), 1);
-        Assert.assertEquals(1106, Proximity.distanceBetween(operaHouse, penguinIsland).inKilometres(), 1);
+        assertEquals(6814.3, Proximity.distanceBetween(bondiBeach, operaHouse).inMetres(), 0.1);
+        assertEquals(1104, Proximity.distanceBetween(bondiBeach, penguinIsland).inKilometres(), 1);
+        assertEquals(1106, Proximity.distanceBetween(operaHouse, penguinIsland).inKilometres(), 1);
     }
 }
